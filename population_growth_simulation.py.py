ce292c196c35b64d7eeb959120f3a1c317592301
Anthony Behnam <anthony.p.behnam@outlook.com>
2024-10-07 02:26:16 -0400

    first commit

---
+++ population_growth_simulation.py.py
@@ -0,0 +1,39 @@
+pop = 312032486
+secInYear = 365 * 24 * 60 * 60
+birth = secInYear // 7
+death = secInYear // 13
+immigrant = secInYear // 45
+pop1 = pop + birth - death + immigrant
+print('Population after 1 year:', pop1)
+
+secIn2Year = secInYear * 2
+birth = secIn2Year // 7
+death = secIn2Year // 13
+immigrant = secIn2Year // 45
+pop2 = pop + birth - death + immigrant
+print('Population after 2 years:',pop2)
+
+secIn3Year = secInYear * 3
+birth = secIn3Year // 7
+death = secIn3Year // 13
+immigrant = secIn3Year // 45
+pop3 = pop + birth - death + immigrant
+print('Population after 3 years:',pop3)
+
+secIn4Year = secInYear * 4
+birth = secIn4Year // 7
+death = secIn4Year // 13
+immigrant = secIn4Year // 45
+pop4 = pop + birth - death + immigrant
+print('Population after 4 years:',pop4)
+
+secIn5Year = secInYear * 5
+birth = secIn5Year // 7
+death = secIn5Year // 13
+immigrant = secIn5Year // 45
+pop5 = pop + birth - death + immigrant
+print('Population after 5 years:',pop5)
+
+
+
+
